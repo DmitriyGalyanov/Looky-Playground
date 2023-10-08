b392846ba29613240e6bbe7c0b50e008492b4b91
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2023-10-08 16:07:21 +0300

    Initial Commit

---
+++ Looky Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Looky Playground
+//
+//  Created by Dmitriy Galyanov on 08.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Looky Playground/Looky_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Looky_PlaygroundApp.swift
+//  Looky Playground
+//
+//  Created by Dmitriy Galyanov on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Looky_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
